3e5d6cd43ce0a16431439b607c72d6f42782aacb
Patrick Miner <patrickminer318@gmail.com>
2017-12-10 12:36:29 -0800

    Setup .env file and added FACEBOOK_APP_ID to the project.  Pivoting to Google Oauth for first experience, will move on to Facebook/Twitter later

--- server/app.js
+++ server/app.js
@@ -1,4 +1,9 @@
 'use strict';
+const passport = require('passport-facebook');
+const dotenv= require('dotenv');
+
+dotenv.config();
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,7 +11,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/login', function(req, res) {
-  console.log('Login path was called');
+  console.log('Login path was called. Here is app ID: ', process.env.FACEBOOK_APP_ID);
   res.send();
 })
 
