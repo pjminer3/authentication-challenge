f9dce9574cb682804ac6a23673f8de23efe6d5fe
Patrick Miner <patrickminer318@gmail.com>
2017-12-10 10:59:58 -0800

    Rendering 'App' component

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World from React</h1>, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello World from React</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
